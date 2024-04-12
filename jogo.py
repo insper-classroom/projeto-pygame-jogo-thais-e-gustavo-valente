9094afab863bd8d222900c949868083664e1d65c
Gustavo Valente <159027272+gustavovvbs@users.noreply.github.com>
2024-04-12 09:31:17 -0300

    primeira vesao

--- jogo.py
+++ jogo.py
@@ -21,7 +21,7 @@ class Dinheiro(pygame.sprite.Sprite):
         self.posicao = pygame.Vector2()
         self.posicao.xy
         self.spritedinheiro = pygame.image.load('money.png')
-        self.spritedinheiro = pygame.transform.scale(spritedinheiro, (50, 80))
+        self.spritedinheiro = pygame.transform.scale(self.spritedinheiro, (50, 80))
 
 class Fundo:
     def __init__(self):
@@ -31,8 +31,8 @@ class Fundo:
 
     def defineSprite(self, corf):
         copia = self.cornormal.copy()
-        cor = colorsys.hsv_to_rgb(tint, 1, 1)
-        copia.fill((cor[0]*80, cor[1]*20, color[2]*70), special_flags = pygame.BLEND_ADD)
+        cor = colorsys.hsv_to_rgb(corf, 1, 1)
+        copia.fill((cor[0]*80, cor[1]*20, cor[2]*70), special_flags = pygame.BLEND_ADD)
         self.spritefundo = copia
     
 class Botao:
@@ -121,7 +121,7 @@ def main():
 
 
 
-        if (clicou and checkCollisions(mx, my, 3, 3, window.get_width() / 2 - botaotentenovamente.get_width() / 2, 288, botaotentenovamente.get_width(), botaotentenovamente.get_height())):
+        if (clicou and checkcolisoes(mx, my, 3, 3, window.get_width() / 2 - botaotentenovamente.get_width() / 2, 288, botaotentenovamente.get_width(), botaotentenovamente.get_height())):
             clicou = False
             break
 
@@ -132,14 +132,14 @@ def main():
 
         pygame.display.update()
 
-        pygame.time.delay()
+        pygame.time.delay(10)
 
     
     #loop do jogoo em si
 
     while True:
 
-        deltat = time.time() - last_time
+        deltat = time.time() - last_updated
         deltat *= 60
         last_updated = time.time()
 
@@ -159,25 +159,25 @@ def main():
             if clicou and my < window.get_height() - 90:
                 pulando = True
 
-            if event.type == QUIT:
+            if event.type == pygame.QUIT:
                 pygame.quit()
 
         
-        camerafora = -jogador.position.y + window.get_height()/2 - jogador.flipatual.get_size()[1]/2
+        camerafora = -jogador.posicao.y + window.get_height()/2 - jogador.flipatual.get_size()[1]/2
 
         window.fill(BRANCO)
         for x in pf:
-            x.defineSprite(((jogaodr.posicao.y/50) % 100) / 100)
+            x.defineSprite(((jogador.posicao.y/50) % 100) / 100)
             window.blit(x.spritefundo, (0, x.posicao))
 
         cor = colorsys.hsv_to_rgb(((jogador.posicao.y / 2) % 100) / 100, 0.2, 0.5)
-        marcadoraltura = font.render(str(altura), True, (0, 0, 255))
-        window.blit(marcadoraltura, (window.get_width()/2 - marcadoraltura.get_width()/2, camerafora + round((jogador.position.y - alturainicial)/window.get_height())*window.get_height() + jogador.flipatual.get_height() - 40))
+        marcadoraltura = font.render(str(altura), True, (0, 255, 0))
+        window.blit(marcadoraltura, (window.get_width()/2 - marcadoraltura.get_width()/2, camerafora + round((jogador.posicao.y - alturainicial)/window.get_height())*window.get_height() + jogador.flipatual.get_height() - 40))
 
         for dinheiro in dinheiros:
             window.blit(dinheiro.spritedinheiro, (dinheiro.posicao.x, dinheiro.posicao.y + camerafora))
 
-        window.blit(pygame.transform.rotate(player.flipatual, clamp(jogador.velocidade.y, -10, 5)*rotacao), (jogador.posicao.x, jogador.posicao.y + camerafora))
+        window.blit(pygame.transform.rotate(jogador.flipatual, clamp(jogador.velocidade.y, -10, 5)*rotacao), (jogador.posicao.x, jogador.posicao.y + camerafora))
         pygame.draw.rect(window, (62, 125, 82), (20, 440, 150*(vida/100), 25))
 
         mostrarcontador = fonte_pequena.render('$' + str(contadordinheiro).zfill(5), True, (0, 0, 0))
@@ -190,10 +190,10 @@ def main():
 
         altura = round(-(jogador.posicao.y - alturainicial) / window.get_height())
 
-        jogador.posicao.x += jogador.velocidade.x*delat 
+        jogador.posicao.x += jogador.velocidade.x*deltat 
         if jogador.flipatual.get_size()[0] + jogador.posicao.x > 640:
             jogador.velocidade.x = -abs(jogador.velocidade.x)
-            jogaodr.flipatual = jogador.viradodireita
+            jogador.flipatual = jogador.viradodireita
             rotacao = 5
 
         if jogador.posicao.x < 0:
@@ -206,8 +206,52 @@ def main():
             pygame.mixer.Sound(sompulo)
 
         jogador.posicao.y += jogador.velocidade.y*deltat
-        jogador.velocidade.y = clamp(jogador.velocidade.y + jogador.aceleracao*dt, -100000000, 50)
+        jogador.velocidade.y = clamp(jogador.velocidade.y + jogador.aceleracao*deltat, -100000000, 50)
+
+        vida -= 0.2*deltat
+        if vida <= 0 and not morto:
+            morto = True
+        
+        for dinheiro in dinheiros:
+            if camerafora + dinheiro.posicao.y + 90 > window.get_height():
+                dinheiro.posicao.y -= window.get_height()*2
+                dinheiro.posicao.x = random.randrange(0, window.get_width() - dinheiro.spritedinheiro.get_width())
+            if (checkcolisoes(jogador.posicao.x, jogador.posicao.y, jogador.flipatual.get_width(), jogador.flipatual.get_height(), dinheiro.posicao.x, dinheiro.posicao.y, dinheiro.spritedinheiro.get_width(), dinheiro.spritedinheiro.get_height())):
+                morto = False
+                contadordinheiro += 1
+                vida = 100
+                dinheiro.posicao.y -= window.get_height() - random.randrange(0, 200)
+                dinheiro.posicao.x = random.randrange(0, window.get_width() - dinheiro.spritedinheiro.get_width())
+                pygame.mixer.Sound.play(somdinheiro)
+
+        if morto and clicou and checkcolisoes(mx, my, 3, 3, 4, 4, botaotentenovamente.get_width(), botaotentenovamente.get_height()):
+            vida = 100
+            contadordinheiro = 0
+            altura = 0
+            forcapulo = 3
+            multiplicadordinheiro = 3
+            jogador.velocidade.xy = 3, 0
+            jogador.posicao.xy = 300, 100
+            jogador.flipatual = jogador.viradoesquerda
+
+            dinheiros = []
+
+            for i in range(5):
+                dinheiros.append(Dinheiro())
+            for dinheiro in dinheiros:
+                dinheiro.posicao.xy = random.randrange(0, window.get_width() - dinheiro.spritedinheiro.get_width()), dinheiros.index(dinheiro)*-200 - jogador.posicao.y
             
+            morto = False
+
+        pf[0].posicao = camerafora + round(jogador.posicao.y / window.get_height())*window.get_height()
+        pf[1].posicao = pf[0].posicao + window.get_height()
+        pf[2].posicao = pf[1].posicao - window.get_height()
+
+        pygame.display.update()
+        pygame.time.delay(10)
+
+if __name__ == '__main__':
+    main()
 
 
 
